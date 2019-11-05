ee8b0d161cef813c8da078d4a07d002437d336cd
unknown <nafreen28@gmail.com>
2019-11-05 15:57:24 +0530

    javaFundamentals added using Eclipse

---
+++ day1/Day-1/javaFundamentals/src/javaFundamentals/Helloworld.java
@@ -0,0 +1,7 @@
+class Helloworld
+{
+public static void main(String a[]) {
+	String.out.println("hellojava");
+
+}
+}
---
+++ day1/Day-1/javaFundamentals/src/javaFundamentals/Helloworld1.java
@@ -0,0 +1,10 @@
+package javaFundamentals;
+
+public class Helloworld1 {
+
+	public static void main(String[] args) {
+		System.out.println("hellojava");
+
+	}
+
+}
